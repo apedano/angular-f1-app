9d33476b40209f0557fc7b817280028bd4e00142
Alessandro Pedano <pedano.alessandro@gmail.com>
2022-10-27 10:29:02 +0200

    PrimeNg now is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,5 @@ import { PrimeNGConfig } from 'primeng/api';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-f1-app';
-
-  constructor(private primengConfig: PrimeNGConfig) { }
-
-  ngOnInit() {
-    this.primengConfig.ripple = true;
-  }
-
+  title = 'primeng-example';
 }
