42f66cd660a31465fc1d379a797469e75cac4697
Alessandro Pedano <pedano.alessandro@gmail.com>
2022-11-07 16:09:56 +0100

    Added image-selector components

--- src/app/team/team.module.ts
+++ src/app/team/team.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { InputTextModule } from "primeng/inputtext";
 import { CalendarModule } from 'primeng/calendar';
-import { FileUploadModule } from 'primeng/fileupload';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSelectCountryModule } from "@angular-material-extensions/select-country";
 import { ReactiveFormsModule } from '@angular/forms';
@@ -19,7 +18,6 @@ import { SharedModule } from "../shared/shared.module";
         InputTextModule,
         CalendarModule,
         BrowserAnimationsModule,
-        FileUploadModule,
         HttpClientModule,
         MatSelectCountryModule,
         ReactiveFormsModule
